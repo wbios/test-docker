8e82cebb51274fa735f9ce5dfd78cfa39ea4f1fb
wbios <kennedy.wrc1@gmail.com>
2026-08-08 23:26:43 +0200

    Aggiunge inserimento utente da Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app =  Flask(__name__)
@@ -20,5 +20,20 @@ def home():
 	
 	return render_template("users.html", users=users)
 
+@app.route("/add", methods=["POST"])
+def add_user():
+	name = request.form["name"]
+
+	conn = get_db_connection()
+	cur = conn.cursor()
+
+	cur.execute("INSERT INTO users (name) VALUES (%s);", (name,))
+	
+	conn.commit()
+
+	cur.close()
+	conn.close()
+
+	return redirect("/")
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
