efeb56c1f2201a80304a889480761aaee83769c1
wbios <kennedy.wrc1@gmail.com>
2026-08-11 23:18:54 +0200

    Test CRUD per la gestione degli utenti

--- conftest.py
+++ conftest.py
@@ -1,5 +1,11 @@
 import pytest
+from db import get_db_connection
 
 @pytest.fixture(autouse=True)
 def test_database(monkeypatch):
 	monkeypatch.setenv("DB_NAME", "testdb")
+
+	with get_db_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+
--- test_app.py
+++ test_app.py
@@ -1,8 +1,71 @@
-import os
-
 from app import app
+from db import get_db_connection
 
 def test_home(monkeypatch):
 	client = app.test_client()
 	response = client.get("/")
 	assert response.status_code == 200
+
+def test_add_user():
+	client = app.test_client()
+	response = client.post(
+		"/add",
+		data={"name" : "Test User"}
+	)
+
+	assert response.status_code == 302
+
+	with get_db_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute(
+				"SELECT name FROM users WHERE name = %s",
+				("Test User",)
+			)
+			user = cur.fetchone()
+	assert user is not None
+	assert user[0] == "Test User"
+
+def test_update_user():
+	client = app.test_client()
+
+	with get_db_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute(
+				"INSERT INTO users (name) VALUES (%s) RETURNING id",
+				("Mario",)
+			)
+			user_id = cur.fetchone()[0]
+
+	response = client.post(
+		f"/update/{user_id}", data={"name":"Mario Updated"}
+	)
+
+	assert response.status_code == 302
+
+	with get_db_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute(
+				"SELECT name FROM users WHERE id = %s", (user_id,)
+			)
+			user = cur.fetchone()
+	assert user is not None
+	assert user[0] == "Mario Updated"
+
+def test_delete_user():
+	client = app.test_client()
+	with get_db_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute(
+				"INSERT INTO users (name) VALUES (%s) RETURNING id", ("User To Delete",)
+			)
+			user_id = cur.fetchone()[0]
+
+	response = client.post(f"/delete/{user_id}")
+	assert response.status_code == 302
+	with get_db_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute(
+				"SELECT id FROM users WHERE id = %s", (user_id,)
+			)
+			user = cur.fetchone()
+	assert user is None
