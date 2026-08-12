5f68526f9a6a8c348c1d532ec4d795245bccaf11
wbios <kennedy.wrc1@gmail.com>
2026-08-12 23:15:40 +0200

    test per i casi di errore CRUD

--- test_app.py
+++ test_app.py
@@ -6,6 +6,23 @@ def test_home(monkeypatch):
 	response = client.get("/")
 	assert response.status_code == 200
 
+def test_add_user_empty_name():
+	client = app.test_client()
+
+	response = client.post(
+		"/add", data={"name":""}
+		)
+
+	assert response.status_code == 302
+
+	with get_db_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute(
+				"SELECT COUNT(*) FROM users"
+			)
+			count = cur.fetchone()[0]
+	assert count == 0
+
 def test_add_user():
 	client = app.test_client()
 	response = client.post(
@@ -45,6 +62,22 @@ def test_update_user(user):
 	assert user is not None
 	assert user[0] == "Mario Updated"
 
+def test_update_user_empty_name(user):
+	user_id = user[0]
+	client = app.test_client()
+	response = client.post(
+		f"/update/{user_id}", data={"name": ""}
+		)
+	assert response.status_code == 302
+
+	with get_db_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute(
+				"SELECT name FROM users WHERE id = %s", (user_id,)
+			)
+			result = cur.fetchone()
+	assert result == ("Mario",)
+
 def test_delete_user(user):
 	user_id = user[0]
 	client = app.test_client()
@@ -60,3 +93,9 @@ def test_delete_user(user):
 			user = cur.fetchone()
 
 	assert user is None
+
+def test_delete_user_not_found():
+	client = app.test_client()
+	response = client.post("/delete/9999")
+	assert response.status_code == 302
+
